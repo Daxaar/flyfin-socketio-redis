a6da3a142617b3062f4c72b9a0dcc5a4d9dedac0
Darren Lewis <darren@octono.com>
2017-12-31 17:49:31 +0000

    Add socket.io

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var socket = require('socket.io');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/sendmessage', function (req, res, next) {
+  console.log('msg is', req.query.msg);
+  console.log(socket);
+
+  socket.emit('news', { message: req.query.msg });
+  //res.status(200).end();
 });
 
 module.exports = router;
